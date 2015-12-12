7aefdb4b92ff511731457e46455608114674e5e9
Eric Lifka <eric.lifka@gmail.com>
2015-12-12 14:29:29 -0500

    parameters w/ defaults

--- src/canvas-engine.js
+++ src/canvas-engine.js
@@ -1,6 +1,10 @@
 window.newCanvasEngine = (function () {
 
-    return function () {
+    return function (options) {
+        var width = options.width || 320;
+        var height = options.height || 200;
+        var container = options.container || document.body;
+
         var el = createCanvasEl();
         var engine = createEngine();
 
