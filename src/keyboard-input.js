2b51963253e687bf9493a809bd9d8a24b40ad26a
Eric Lifka <eric.lifka@gmail.com>
2015-12-14 19:38:17 -0500

    the keys we care about for now

--- src/keyboard-input.js
+++ src/keyboard-input.js
@@ -1,11 +1,16 @@
  window.newKeyboardInputManager = (function () {
 
+     var KEYS = {
+         W: 87, A: 65, S: 83, D: 68,
+         SPACE: 32
+     };
+
      return function () {
          document.body.addEventListener('keydown', function (event) {
-             console.log('down', event);
+             console.log('down', event.keyCode);
          });
          document.body.addEventListener('keyup', function (event) {
-             console.log('up', event);
+            //  console.log('up', event);
          });
      };
  }());
