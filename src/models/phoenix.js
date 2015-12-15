5055c603daaa7a9105283287c41bc994437c9c46
Eric Lifka <eric.lifka@gmail.com>
2015-12-15 01:48:52 -0500

    made a dot

--- src/models/phoenix.js
+++ src/models/phoenix.js
@@ -1,11 +1,16 @@
 window.newPhoenixModel = (function () {
-    function Phoenix() { }
+    function Phoenix() {
+        this.position_x = 10;
+        this.position_y = 10;
+    }
+
     Phoenix.prototype = {
         processInput: function () {
         },
         update: function () {
         },
-        renderToFrame: function () {
+        renderToFrame: function (frame) {
+            frame.cells[ this.position_x ][ this.position_y ].color = "white";
         }
     };
 
