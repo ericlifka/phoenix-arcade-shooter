eabc8e972c74bba62b028122b0b92602f630bfbb
Eric Lifka <eric.lifka@gmail.com>
2015-12-14 20:05:31 -0500

    some fanciness to make sure key presses don't get dropped if they're within one frame

--- src/keyboard-input.js
+++ src/keyboard-input.js
@@ -1,34 +1,50 @@
  window.newKeyboardInputManager = (function () {
 
+      function cloneObj(obj) {
+          var nObj = { };
+          Object.keys(obj).forEach(function (key) {
+              nObj[ key ] = obj[ key ];
+          });
+          return nObj;
+      }
+      function newInputDescriptor() {
+          return {
+              W: false, A: false, S: false, D: false,
+              SPACE: false
+          };
+      }
+      function propagateInputClears() {
+          Object.keys(clearAfterNext).forEach(function (key) {
+              if (clearAfterNext[ key ]) {
+                  inputState[ key ] = false;
+                  clearAfterNext[ key ] = false;
+              }
+          });
+      }
+
      var KEYS = {
          87: W, 65: A, 83: S, 68: D,
          32: SPACE
      };
 
-     var inputState = {
-         W: false, A: false, S: false, D: false,
-         SPACE: false
-     }
+     var inputState = newInputDescriptor();
+     var clearAfterNext = newInputDescriptor();
 
-     function cloneObj(obj) {
-         var nObj = { };
-         Object.keys(obj).forEach(function (key) {
-             nObj[ key ] = obj[ key ];
-         });
-         return nObj;
-     }
 
      return function () {
          document.body.addEventListener('keydown', function (event) {
              inputState[ KEYS[ event.keyCode ] ] = true;
+             clearAfterNext[ KEYS[ event.keyCode ] ] = false;
          });
          document.body.addEventListener('keyup', function (event) {
-            inputState[ KEYS[ event.keyCode ] ] = false;
+            clearAfterNext[ KEYS[ event.keyCode ] ] = true;
          });
 
          return {
              getInputState: function () {
-                 return cloneObj(inputState);
+                 var state = cloneObj(inputState);
+                 propagateInputClears();
+                 return state;
              }
          };
      };
