557941bc79f4278404962af5c73236b178e17812
Eric Lifka <eric.lifka@gmail.com>
2015-12-15 01:47:31 -0500

    remove random blue pixels

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -20,10 +20,6 @@ window.newGameController = (function () {
             this.model.update(dtime);
             this.model.renderToFrame(frame);
 
-            var x = Math.floor(Math.random() * (frame.width));
-            var y = Math.floor(Math.random() * (frame.height));
-            frame.cells[ x ][ y ].color = "blue";
-
             this.renderer.renderFrame(frame);
         },
         processInput: function (inputState) {
