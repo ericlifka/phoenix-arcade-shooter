999520ddb4f371005f8f0ed29d3e2a25f1a11f14
Eric Lifka <eric.lifka@gmail.com>
2015-12-17 15:19:03 -0500

    bumping up the OO patterns

--- src/helpers/mixin.js
+++ src/helpers/mixin.js
@@ -3,3 +3,21 @@ window.MixIn = function (_class, properties) {
         _class.prototype[ key ] = properties[ key ];
     });
 };
+
+window.DefineClass = function (Base, definition) {
+    if (typeof Base === "object" && !definition) {
+        definition = Base;
+        Base = function () { };
+    }
+
+    function Constructor() {
+        if (typeof this.constructor === "function") {
+            this.constructor.apply(this, arguments);
+        }
+    }
+
+    Constructor.prototype = new Base();
+    MixIn(Constructor, definition);
+
+    return Constructor;
+};
