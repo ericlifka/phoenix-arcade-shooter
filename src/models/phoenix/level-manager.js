475688a7628b079d1470f1052af5263092c5e0a5
Eric Lifka <eric.lifka@gmail.com>
2015-12-18 15:54:47 -0500

    starting to make some sense

--- src/models/phoenix/level-manager.js
+++ src/models/phoenix/level-manager.js
@@ -1,18 +1,34 @@
-window.PhoenixLevelManager = DefineClass({
-    levels: [
-        {
+(function () {
+
+    var FlyPlayerInFromBottom = DefineClass({
+        constructor: function (game) {
+            this.game = game;
+            this.player = game.player;
+        },
+        start: function () {
+
+        },
+        update: function (dtime) {
 
         }
-    ],
-    constructor: function (game) {
-        this.game = game;
-        this.activeScripts = [ ];
-        this.nextLevel = 0;
-    },
-    startLevel: function () {
-        this.currentLevel = this.levels[ this.nextLevel ];
-        this.nextLevel++;
+    });
 
-        
-    }
-});
+    window.PhoenixLevelManager = DefineClass({
+        levels: [
+            {
+
+            }
+        ],
+        constructor: function (game) {
+            this.game = game;
+            this.activeScripts = [ ];
+            this.nextLevel = 0;
+        },
+        startLevel: function () {
+            this.currentLevel = this.levels[ this.nextLevel ];
+            this.nextLevel++;
+
+            this.activeScripts.push(new FlyPlayerInFromBottom(this.game));
+        }
+    });
+}());
