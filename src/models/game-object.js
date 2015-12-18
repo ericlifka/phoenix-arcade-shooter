e0ac86864ed0dcfb2b7ade8e14b5a8f002b29bce
Eric Lifka <eric.lifka@gmail.com>
2015-12-18 16:08:15 -0500

    safe to reference children because the constructor is always called via super

--- src/models/game-object.js
+++ src/models/game-object.js
@@ -4,22 +4,18 @@ window.GameObject = DefineClass({
         this.children = [];
     },
     processInput: function (input) {
-        if (this.children) {
-            this.children.forEach(function (child) {
-                if (typeof child.processInput === "function") {
-                    child.processInput(input);
-                }
-            });
-        }
+        this.children.forEach(function (child) {
+            if (typeof child.processInput === "function") {
+                child.processInput(input);
+            }
+        });
     },
     update: function (dtime) {
-        if (this.children) {
-            this.children.forEach(function (child) {
-                if (typeof child.update === "function") {
-                    child.update(dtime);
-                }
-            });
-        }
+        this.children.forEach(function (child) {
+            if (typeof child.update === "function") {
+                child.update(dtime);
+            }
+        });
 
         if (this.position && this.velocity) {
             this.position.x += this.velocity.x * dtime / 1000;
@@ -30,25 +26,23 @@ window.GameObject = DefineClass({
     },
     checkBoundaries: function () { },
     renderToFrame: function (frame) {
-        if (this.children) {
-            this.children.forEach(function (child) {
-                if (typeof child.renderToFrame === "function") {
-                    child.renderToFrame(frame);
-                }
-            });
-        }
+        this.children.forEach(function (child) {
+            if (typeof child.renderToFrame === "function") {
+                child.renderToFrame(frame);
+            }
+        });
 
         if (this.sprite && this.position) {
             this.sprite.renderToFrame(Math.floor(this.position.x), Math.floor(this.position.y), frame);
         }
     },
     addChild: function (child) {
-        if (this.children && child) {
+        if (child) {
             this.children.push(child);
         }
     },
     removeChild: function (child) {
-        if (this.children && child) {
+        if (child) {
             var index = this.children.indexOf(child);
             if (index >= 0) {
                 this.children.splice(index, 1);
