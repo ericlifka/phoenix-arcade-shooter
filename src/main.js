941969def743e006d935c3aff60e187d39f12ec5
Eric Lifka <eric.lifka@gmail.com>
2015-12-21 17:11:20 -0500

    don't need those variables split out any more

--- src/main.js
+++ src/main.js
@@ -3,17 +3,12 @@ DefineModule('main', function (require) {
     var newRunLoop = require('helpers/run-loop');
     var newKeyboardInputController = require('helpers/keyboard-input');
     var GameController = require('controllers/game');
-
     var gameDimensions = { width: 100, height: 75 };
-    var gameModel = newPhoenixModel(gameDimensions);
-    var gameView = newCanvasRenderer(gameDimensions);
-    var runLoop = newRunLoop();
-    var inputController = newKeyboardInputController();
 
     window.activeGame = new GameController({
-        renderer: gameView,
-        input: inputController,
-        runLoop: runLoop,
-        model: gameModel
+        renderer: newCanvasRenderer(gameDimensions),
+        input: newKeyboardInputController(),
+        runLoop: newRunLoop(),
+        model: newPhoenixModel(gameDimensions)
     });
 });
