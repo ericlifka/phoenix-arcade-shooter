588d5d876d7c5972f9baef78e6145761743a3e08
Eric Lifka <eric.lifka@gmail.com>
2015-12-20 23:58:03 -0500

    put the modules somewhere

--- modules.js
+++ modules.js
@@ -1,5 +1,13 @@
 (function () {
+    var moduleDefinitions = { };
+
     window.DefineModule = function (moduleName, moduleDefinition) {
         console.log('registering ' + moduleName);
+
+        if (moduleDefinitions[ moduleName ]) {
+            throw "Duplicate module definition: " + moduleName;
+        }
+
+        moduleDefinitions[ moduleName ] = moduleDefinition;
     };
 }());
