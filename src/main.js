04d61dc4261155873f9cbba049215ce0fa8e8327
Eric Lifka <eric.lifka@gmail.com>
2015-12-21 16:02:53 -0500

    going to try to do this stepwise, it may not work past this step

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+DefineModule('main', function () {
     var gameDimensions = { width: 100, height: 75 };
     var gameModel = newPhoenixModel(gameDimensions);
     var gameView = newCanvasRenderer(gameDimensions);
