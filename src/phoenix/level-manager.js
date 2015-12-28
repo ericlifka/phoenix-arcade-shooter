8df1849d2f27cc125eeb3f6d9e4d385b0cbeec58
Eric Lifka <eric.lifka@gmail.com>
2015-12-28 10:39:25 -0500

    re-arranging files and pulling things out by them selves

--- src/phoenix/level-manager.js
+++ src/phoenix/level-manager.js
@@ -1,39 +1,7 @@
 DefineModule('phoenix/level-manager', function (require) {
     var GameObject = require('models/game-object');
     var EnemyShip = require('phoenix/enemy-ship');
-
-    var FlyPlayerInFromBottom = DefineClass(GameObject, {
-        TIME_STEP: 100,
-        ELAPSED: 0,
-        constructor: function (parent, game) {
-            this.super('constructor', arguments);
-
-            this.game = game;
-            this.player = game.player;
-        },
-        start: function () {
-            this.player.preventInputControl = true;
-
-            this.setStartingPosition();
-        },
-        update: function (dtime) {
-            this.super('update', arguments);
-
-            if (this.player.position.y < this.game.height - this.player.sprite.height - 2) {
-                this.player.preventInputControl = false;
-                this.parent.signalScriptFinished(this);
-            }
-        },
-        setStartingPosition: function () {
-            var position = this.player.position;
-            var velocity = this.player.velocity;
-
-            position.x = Math.floor(this.game.width / 2 - this.player.sprite.width / 2);
-            position.y = this.game.height + 2;
-            velocity.x = 0;
-            velocity.y = -this.player.SPEED / 5;
-        }
-    });
+    var FlyPlayerInFromBottom = require('phoenix/scripts/fly-player-in-from-bottom');
 
     var MoveObjectToPoint = DefineClass(GameObject, {
         active: false,
---
+++ src/phoenix/scripts/fly-player-in-from-bottom.js
@@ -0,0 +1,36 @@
+DefineModule('phoenix/scripts/fly-player-in-from-bottom', function (require) {
+    var GameObject = require('models/game-object');
+
+    return DefineClass(GameObject, {
+        TIME_STEP: 100,
+        ELAPSED: 0,
+        constructor: function (parent, game) {
+            this.super('constructor', arguments);
+
+            this.game = game;
+            this.player = game.player;
+        },
+        start: function () {
+            this.player.preventInputControl = true;
+
+            this.setStartingPosition();
+        },
+        update: function (dtime) {
+            this.super('update', arguments);
+
+            if (this.player.position.y < this.game.height - this.player.sprite.height - 2) {
+                this.player.preventInputControl = false;
+                this.parent.signalScriptFinished(this);
+            }
+        },
+        setStartingPosition: function () {
+            var position = this.player.position;
+            var velocity = this.player.velocity;
+
+            position.x = Math.floor(this.game.width / 2 - this.player.sprite.width / 2);
+            position.y = this.game.height + 2;
+            velocity.x = 0;
+            velocity.y = -this.player.SPEED / 5;
+        }
+    });
+});
