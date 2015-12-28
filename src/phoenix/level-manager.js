51f4bbaa39235837723ef6cf9f5900680e8b283d
Eric Lifka <eric.lifka@gmail.com>
2015-12-28 10:52:49 -0500

    levels too, cause why not

--- src/phoenix/level-manager.js
+++ src/phoenix/level-manager.js
@@ -1,41 +1,7 @@
 DefineModule('phoenix/level-manager', function (require) {
     var GameObject = require('models/game-object');
-    var EnemyShip = require('phoenix/enemy-ship');
     var FlyPlayerInFromBottom = require('phoenix/scripts/fly-player-in-from-bottom');
-
-    var MoveObjectToPoint = require('phoenix/scripts/move-object-to-point');
-
-    var LevelOneEnemies = DefineClass(GameObject, {
-        speed: 10,
-        constructor: function (parent, game) {
-            this.super('constructor', arguments);
-
-            this.game = game;
-            this.ships = [];
-
-            for (var i = 1; i <= 10; i++) {
-                var ship = new EnemyShip(game);
-                ship.position.x = 10 * i;
-                ship.position.y = -20 * i;
-
-                this.addChild(new MoveObjectToPoint(game, ship, {
-                    x: ship.position.x,
-                    y: 20
-                }, 2 + i * .5));
-                this.ships.push(ship);
-            }
-        },
-        start: function () {
-            var game = this.game;
-            this.ships.forEach(function (ship) {
-                game.addChild(ship);
-            });
-
-            this.children.forEach(function (script) {
-                script.start();
-            });
-        }
-    });
+    var LevelOneEnemies = require('phoenix/levels/level-01');
 
     return DefineClass(GameObject, {
         levels: [
---
+++ src/phoenix/levels/level-01.js
@@ -0,0 +1,37 @@
+DefineModule('phoenix/levels/level-01', function (require) {
+    var EnemyShip = require('phoenix/enemy-ship');
+    var GameObject = require('models/game-object');
+    var MoveObjectToPoint = require('phoenix/scripts/move-object-to-point');
+
+    return DefineClass(GameObject, {
+        speed: 10,
+        constructor: function (parent, game) {
+            this.super('constructor', arguments);
+
+            this.game = game;
+            this.ships = [];
+
+            for (var i = 1; i <= 10; i++) {
+                var ship = new EnemyShip(game);
+                ship.position.x = 10 * i;
+                ship.position.y = -20 * i;
+
+                this.addChild(new MoveObjectToPoint(game, ship, {
+                    x: ship.position.x,
+                    y: 20
+                }, 2 + i * .5));
+                this.ships.push(ship);
+            }
+        },
+        start: function () {
+            var game = this.game;
+            this.ships.forEach(function (ship) {
+                game.addChild(ship);
+            });
+
+            this.children.forEach(function (script) {
+                script.start();
+            });
+        }
+    });
+});
