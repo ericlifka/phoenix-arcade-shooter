f283cde2df65f6426a1397d98d465fa17ff3f7b1
Eric Lifka <eric.lifka@gmail.com>
2015-12-27 20:40:22 -0500

    make a new sprite for the first level, easier to hit

--- src/phoenix/enemy-ship.js
+++ src/phoenix/enemy-ship.js
@@ -1,12 +1,12 @@
 DefineModule('phoenix/enemy-ship', function (require) {
     var GameObject = require('models/game-object');
-    var shipSprite = require('phoenix/sprites/dagger-ship');
+    var shipSprite = require('phoenix/sprites/arrow-ship');
 
     return DefineClass(GameObject, {
         constructor: function () {
             this.super('constructor', arguments);
 
-            this.sprite = shipSprite().rotateLeft();
+            this.sprite = shipSprite().rotateRight();
             this.position = { x: 0, y: 0 };
             this.velocity = { x: 0, y: 0 };
         }
---
+++ src/phoenix/sprites/arrow-ship.js
@@ -0,0 +1,18 @@
+DefineModule('phoenix/sprites/arrow-ship', function (require) {
+    var Sprite = require('models/sprite');
+
+    return function () {
+        var w = "white";
+        var n = null;
+        return new Sprite([
+            [w, n, n, n, n, n, w],
+            [w, w, n, n, n, w, w],
+            [n, w, w, n, w, w, n],
+            [n, w, w, w, w, w, n],
+            [n, n, w, w, w, n, n],
+            [n, n, w, w, w, n, n],
+            [n, n, n, w, n, n, n],
+            [n, n, n, w, n, n, n]
+        ]);
+    }
+});
