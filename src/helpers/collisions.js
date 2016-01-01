36fe2187ed674ba9277b686a6e0aa584391e326f
Eric Lifka <eric.lifka@gmail.com>
2015-12-31 20:57:08 -0500

    easy implementation of bounding box collision

--- src/helpers/collisions.js
+++ src/helpers/collisions.js
@@ -1,7 +1,24 @@
 DefineModule('helpers/collisions', function (require) {
+    function entityToBoundingBox(entity) {
+        return {
+            x1: entity.position.x,
+            x2: entity.position.x + entity.sprite.width,
+            y1: entity.position.y,
+            y2: entity.position.y + entity.sprite.height
+        };
+    }
+
     return {
         boxCollision: function (entityA, entityB) {
-            return false;
+            var a = entityToBoundingBox(entityA);
+            var b = entityToBoundingBox(entityB);
+
+            return (
+                a.x1 < b.x2 &&
+                a.x2 > b.x1 &&
+                a.y1 < b.y2 &&
+                a.y2 > b.y1
+            );
         },
         spriteCollision: function (entityA, entityB) {
             return false;
