2515a3c5979daa34f33fd3e99544304811ca5687
Eric Lifka <eric.lifka@gmail.com>
2016-01-03 00:13:25 -0500

    inherit from gameobject so that the animation recieves updates

--- src/models/animation.js
+++ src/models/animation.js
@@ -1,7 +1,13 @@
 DefineModule('models/animation', function (require) {
-    return DefineClass({
+    var GameObject = require('models/game-object');
+
+    return DefineClass(GameObject, {
         constructor: function (frames) {
             this.frames = frames;
+            this.currentFrame = 0;
+
+            this.width = frames[ 0 ].length;
+            this.height = frames[ 0 ].length;
         },
         renderToFrame: function (x, y, frame) {
 
