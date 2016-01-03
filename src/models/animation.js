8cadbcff945329a422259d31463d1b2771148abe
Eric Lifka <eric.lifka@gmail.com>
2016-01-03 01:03:03 -0500

    slow animations down by taking time elapsed into account when switching frames

--- src/models/animation.js
+++ src/models/animation.js
@@ -8,18 +8,22 @@ DefineModule('models/animation', function (require) {
 
             this.width = this.frames[ 0 ].length;
             this.height = this.frames[ 0 ].length;
-
+            this.millisEllapsedOnFrame = 0;
         },
         update: function (dtime) {
-            console.log('animation update', dtime);
+            this.millisEllapsedOnFrame += dtime;
+
+            if (this.millisEllapsedOnFrame >= this.millisPerFrame) {
+                this.millisEllapsedOnFrame -= this.millisPerFrame;
+
+                this.currentFrame += 1;
+                if (this.currentFrame >= this.frames.length) {
+                    this.currentFrame = 0;
+                }
+            }
         },
         renderToFrame: function (x, y, frame) {
             this.frames[ this.currentFrame ].renderToFrame(x, y, frame);
-
-            this.currentFrame += 1;
-            if (this.currentFrame >= this.frames.length) {
-                this.currentFrame = 0;
-            }
         }
     });
 });
