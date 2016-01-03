b247a4ca4f13ae649455ca7912228a5d71a4bcb2
Eric Lifka <eric.lifka@gmail.com>
2016-01-02 20:06:21 -0500

    stub the animation class and import the files

---
+++ src/models/animation.js
@@ -0,0 +1,10 @@
+DefineModule('models/animation', function (require) {
+    return DefineClass({
+        constructor: function (frames) {
+            this.frames = frames;
+        },
+        renderToFrame: function (x, y, frame) {
+
+        }
+    });
+});
