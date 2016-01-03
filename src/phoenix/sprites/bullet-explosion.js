c188ef0d74e9b51b32b2f5580d152cc464b6976e
Eric Lifka <eric.lifka@gmail.com>
2016-01-02 19:39:47 -0500

    stub out what an animation could look like

---
+++ src/phoenix/sprites/bullet-explosion.js
@@ -0,0 +1,32 @@
+DefineModule('phoenix/sprites/bullet-explosion', function (require) {
+    var Animation = require('models/animation');
+    var Sprite = require('models/sprite');
+
+    var n = null;
+    var y = "yellow";
+
+    var frames = [
+
+        new Sprite([
+            [n, n, n],
+            [n, y, n],
+            [n, n, n]
+        ]),
+        new Sprite([
+            [n, y, n],
+            [y, n, y],
+            [n, y, n]
+        ]),
+        new Sprite([
+            [y, n, y],
+            [n, n, n],
+            [y, n, y]
+        ])
+
+    ];
+
+
+    return function () {
+        return new Animation(frames);
+    }
+});
