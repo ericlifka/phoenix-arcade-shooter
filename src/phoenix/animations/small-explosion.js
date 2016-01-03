04670fc115cd0bc32aca234e15f90140658e0fa0
Eric Lifka <eric.lifka@gmail.com>
2016-01-03 01:17:28 -0500

    make the animation a bit fuller

--- src/phoenix/animations/small-explosion.js
+++ src/phoenix/animations/small-explosion.js
@@ -4,25 +4,50 @@ DefineModule('phoenix/animations/small-explosion', function (require) {
 
     var n = null;
     var y = "yellow";
+    var o = "orange";
+    var r = "red";
 
     var frames = [
 
         new Sprite([
-            [n, n, n],
-            [n, y, n],
-            [n, n, n]
+            [n, n, n, n, n],
+            [n, n, n, n, n],
+            [n, n, r, n, n],
+            [n, n, n, n, n],
+            [n, n, n, n, n]
         ]),
-        new Sprite([
-            [n, y, n],
-            [y, n, y],
-            [n, y, n]
-        ]),
-        new Sprite([
-            [y, n, y],
-            [n, n, n],
-            [y, n, y]
-        ])
 
+        new Sprite([
+            [n, n, n, n, n],
+            [n, n, o, n, n],
+            [n, o, o, o, n],
+            [n, n, o, n, n],
+            [n, n, n, n, n]
+        ]),
+
+        new Sprite([
+            [n, n, y, n, n],
+            [n, y, y, y, n],
+            [y, y, y, y, y],
+            [n, y, y, y, n],
+            [n, n, y, n, n]
+        ]),
+
+        new Sprite([
+            [n, n, y, n, n],
+            [n, y, y, y, n],
+            [y, y, n, y, y],
+            [n, y, y, y, n],
+            [n, n, y, n, n]
+        ]),
+
+        new Sprite([
+            [n, n, y, n, n],
+            [n, y, n, y, n],
+            [y, n, n, n, y],
+            [n, y, n, y, n],
+            [n, n, y, n, n]
+        ])
     ];
 
 
