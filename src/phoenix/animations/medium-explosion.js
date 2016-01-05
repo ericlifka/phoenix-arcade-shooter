e9240afee6959d5fe769d6d7fe568ebc85b651cd
Eric Lifka <eric.lifka@gmail.com>
2016-01-05 00:01:32 -0500

    add some randomness to the positioning of the explosions

--- src/phoenix/animations/medium-explosion.js
+++ src/phoenix/animations/medium-explosion.js
@@ -1,19 +1,23 @@
 DefineModule('phoenix/animations/medium-explosion', function (require) {
-    var SpriteGroup = require('models/sprite-group');
+    var Random = require('helpers/random');
     var smallExplosion = require('phoenix/animations/small-explosion');
+    var SpriteGroup = require('models/sprite-group');
 
     return function () {
         return new SpriteGroup([
             {
-                x: 0, y: 0,
+                x: 0,
+                y: Random.integer(0, 3),
                 sprite: smallExplosion()
             },
             {
-                x: 5, y: 0,
+                x: Random.integer(3, 6),
+                y: 0,
                 sprite: smallExplosion()
             },
             {
-                x: 2, y: 5,
+                x: Random.integer(2, 4),
+                y: Random.integer(4, 6),
                 sprite: smallExplosion()
             }
         ]);
