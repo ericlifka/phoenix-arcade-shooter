ede0805aef3e9981b0f25409123609e96cca02fe
Eric Lifka <eric.lifka@gmail.com>
2016-01-04 23:39:49 -0500

    grab the width and height from the largest dimension of the sub sprites

--- src/models/sprite-group.js
+++ src/models/sprite-group.js
@@ -1,6 +1,18 @@
 DefineModule('models/sprite-group', function (require) {
     return DefineClass({
-        constructor: function () {
+        constructor: function (sprites) {
+            this.spriteDescriptors = sprites || [];
+
+            this.width = Math.max.apply(null, this.spriteDescriptors.map(function (descriptor) {
+                return descriptor.x + descriptor.sprite.width;
+            }));
+
+            this.height = Math.max.apply(null, this.spriteDescriptors.map(function (descriptor) {
+                return descriptor.y + descriptor.sprite.height;
+            }));
+        },
+
+        renderToFrame: function (x, y, frame) {
 
         }
     });
