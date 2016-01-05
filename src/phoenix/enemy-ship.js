cbefd9bbeccde0ebdbb3368d0044e1a287cc7896
Eric Lifka <eric.lifka@gmail.com>
2016-01-04 23:21:31 -0500

    really easy to drop in the damage code from the bullet, but it will need tweaks

--- src/phoenix/enemy-ship.js
+++ src/phoenix/enemy-ship.js
@@ -1,6 +1,7 @@
 DefineModule('phoenix/enemy-ship', function (require) {
     var GameObject = require('models/game-object');
     var shipSprite = require('phoenix/sprites/arrow-ship');
+    var smallExplosion = require('phoenix/animations/small-explosion');
 
     return DefineClass(GameObject, {
         constructor: function () {
@@ -9,6 +10,15 @@ DefineModule('phoenix/enemy-ship', function (require) {
             this.sprite = shipSprite().rotateRight();
             this.position = { x: 0, y: 0 };
             this.velocity = { x: 0, y: 0 };
+        },
+        applyDamage: function (damage) {
+            this.exploding = true;
+            this.sprite = smallExplosion();
+
+            this.velocity.x = 0;
+            this.velocity.y = 0;
+            this.position.x -= Math.floor(this.sprite.width / 2);
+            this.position.y -= Math.floor(this.sprite.height / 2);
         }
     });
 });
