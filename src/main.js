10bfa7aea46a9e007ca59f7e7f931070e6430973
Eric Lifka <eric.lifka@gmail.com>
2016-01-06 10:29:12 -0500

    alphabetize that shit

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 DefineModule('main', function (require) {
     var CanvasRenderer = require('views/canvas-renderer');
-    var RunLoop = require('helpers/run-loop');
-    var KeyboardController = require('controllers/keyboard-input');
-    var GamepadController = require('controllers/gamepad-input');
     var GameController = require('controllers/game');
+    var GamepadController = require('controllers/gamepad-input');
+    var KeyboardController = require('controllers/keyboard-input');
     var Phoenix = require('phoenix/game');
+    var RunLoop = require('helpers/run-loop');
 
     var gameDimensions = { width: 200, height: 150 };
 
     window.activeGame = new GameController({
-        renderer: new CanvasRenderer(gameDimensions),
         inputSources: [
             new KeyboardController(),
             new GamepadController()
         ],
-        runLoop: new RunLoop(),
-        model: new Phoenix(gameDimensions)
+        model: new Phoenix(gameDimensions),
+        renderer: new CanvasRenderer(gameDimensions),
+        runLoop: new RunLoop()
     });
 });
