b98ea6e3f80832989cd017491838db07ab93ab84
Eric Lifka <eric.lifka@gmail.com>
2016-01-05 23:58:59 -0500

    map of the button indexes to what button they are

--- src/controllers/gamepad-input.js
+++ src/controllers/gamepad-input.js
@@ -1,4 +1,24 @@
 DefineModule('controllers/gamepad-input', function (require) {
+
+    var BUTTON_MAP = {
+        0: 'A',
+        1: 'B',
+        2: 'X',
+        3: 'Y',
+        4: 'left-bumper',
+        5: 'right-bumper',
+        6: 'left-trigger',
+        7: 'right-trigger',
+        8: 'back',
+        9: 'start',
+        10: 'left-stick-press',
+        11: 'right-stick-press',
+        12: 'd-pad-up',
+        13: 'd-pad-down',
+        14: 'd-pad-left',
+        15: 'd-pad-right'
+    };
+
     return DefineClass({
         constructor: function () {
             window.addEventListener("gamepadconnected", function(e) {
@@ -17,8 +37,9 @@ DefineModule('controllers/gamepad-input', function (require) {
 
             if (gamepad && gamepad.connected) {
                 gamepad.buttons.forEach(function (button, index) {
-                    if (button.pressed || button.value == 1.0) {
-                        console.log(index);
+                    if (button.pressed) {
+                        console.log(BUTTON_MAP[ index ], button.value);
+                        
                     }
                 });
             }
