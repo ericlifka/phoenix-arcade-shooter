3fcb5ec110a86432986fb4b6e40ea53939d541c6
Eric Lifka <eric.lifka@gmail.com>
2016-01-07 15:06:09 -0500

    descriptor object that better represents the input types in phoenix

--- src/phoenix/input-interpretter.js
+++ src/phoenix/input-interpretter.js
@@ -1,11 +1,21 @@
 DefineModule('phoenix/input-interpretter', function (require) {
+
+    function newInputDescriptor() {
+        return {
+            GAME: 'phoenix',
+            movementVector: { x: 0, y: 0 },
+            fire: false
+        };
+    }
+
     return DefineClass({
         interpret: function (inputSources) {
+            var gameInput = newInputDescriptor();
 
             inputSources.forEach(function (inputSource) {
                 switch(inputSource.INPUT_TYPE) {
-                    case 'gamepad': return this.addGamepadInput(inputSource);
-                    case 'keyboard': return this.addKeyboardInput(inputSource);
+                    case 'gamepad': return this.addGamepadInput(inputSource, gameInput);
+                    case 'keyboard': return this.addKeyboardInput(inputSource, gameInput);
                     default:
                         console.error("Unsupported input type: ", inputSource.INPUT_TYPE);
                 }
