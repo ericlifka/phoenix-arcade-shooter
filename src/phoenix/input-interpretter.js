86d522940997f7e8d0da9e98b5ab3da5fd5c3238
Eric Lifka <eric.lifka@gmail.com>
2016-01-07 14:55:04 -0500

    loop through input sources to pull input out of them

--- src/phoenix/input-interpretter.js
+++ src/phoenix/input-interpretter.js
@@ -1,6 +1,16 @@
 DefineModule('phoenix/input-interpretter', function (require) {
     return DefineClass({
         interpret: function (inputSources) {
+
+            inputSources.forEach(function (inputSource) {
+                switch(inputSource.INPUT_TYPE) {
+                    case 'gamepad': return this.addGamepadInput(inputSource);
+                    case 'keyboard': return this.addKeyboardInput(inputSource);
+                    default:
+                        console.error("Unsupported input type: ", inputSource.INPUT_TYPE);
+                }
+            }.bind(this));
+
             return null;
         }
     });
