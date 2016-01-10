04777209a54fc821e6712c2fb6f6ebf40dc8184f
Eric Lifka <eric.lifka@gmail.com>
2016-01-10 17:13:00 -0500

    test message

--- src/phoenix/level-manager.js
+++ src/phoenix/level-manager.js
@@ -1,6 +1,7 @@
 DefineModule('phoenix/level-manager', function (require) {
     var GameObject = require('models/game-object');
     var Level_01 = require('phoenix/levels/level-01');
+    var TextDisplay = require('models/text-display');
 
     return DefineClass(GameObject, {
         levels: [null, Level_01],
@@ -18,6 +19,10 @@ DefineModule('phoenix/level-manager', function (require) {
             this.currentLevel = new LevelClass(this, this.game);
             this.addChild(this.currentLevel);
             this.currentLevel.start();
+
+            this.addChild(new TextDisplay(this, {
+                message: "bob"
+            }));
         },
         update: function () {
             this.super('update', arguments);
