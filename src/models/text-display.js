b4aade7587e722cb9eedb49ac8f10b796135611c
Eric Lifka <eric.lifka@gmail.com>
2016-01-10 15:35:36 -0500

    make a class that can display text on the screen

---
+++ src/models/text-display.js
@@ -0,0 +1,9 @@
+DefineModule('models/text-display', function (require) {
+    var GameObject = require('models/game-object');
+    
+    return DefineClass(GameObject, {
+        constructor: function (parent) {
+
+        }
+    });
+});
