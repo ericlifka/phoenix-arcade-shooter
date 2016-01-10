523ba8b116f79fc65de0cfc547624a49fd954240
Eric Lifka <eric.lifka@gmail.com>
2016-01-10 16:58:57 -0500

    pass message as an options obj

--- src/models/text-display.js
+++ src/models/text-display.js
@@ -1,9 +1,11 @@
 DefineModule('models/text-display', function (require) {
     var GameObject = require('models/game-object');
-    
-    return DefineClass(GameObject, {
-        constructor: function (parent) {
 
+    return DefineClass(GameObject, {
+        constructor: function (parent, options) {
+            this.super('constructor', arguments);
+
+            this.message = options.message || "";
         }
     });
 });
