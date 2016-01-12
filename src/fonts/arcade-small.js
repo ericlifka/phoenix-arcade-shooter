8ddaccad75178dfe855015110c50dc77004aa3c7
Eric Lifka <eric.lifka@gmail.com>
2016-01-11 23:54:18 -0500

    capital letters done

--- src/fonts/arcade-small.js
+++ src/fonts/arcade-small.js
@@ -17,6 +17,137 @@ DefineModule('fonts/arcade-small', function (require) {
             [w,n,w,n,n],
             [n,w,w,w,w]
         ]),
+        B: new Sprite([
+            [w,w,w,w,w],
+            [w,n,w,n,w],
+            [n,w,n,w,n]
+        ]),
+        C: new Sprite([
+            [n,w,w,w,n],
+            [w,n,n,n,w],
+            [n,w,n,w,n]
+        ]),
+        D: new Sprite([
+            [w,w,w,w,w],
+            [w,n,n,n,w],
+            [n,w,w,w,n]
+        ]),
+        E: new Sprite([
+            [w,w,w,w,w],
+            [w,n,w,n,w],
+            [w,n,n,n,w]
+        ]),
+        F: new Sprite([
+            [w,w,w,w,w],
+            [w,n,w,n,n],
+            [w,n,n,n,n]
+        ]),
+        G: new Sprite([
+            [n,w,w,w,n],
+            [w,n,n,n,w],
+            [w,n,n,w,w]
+        ]),
+        H: new Sprite([
+            [w,w,w,w,w],
+            [n,n,w,n,n],
+            [w,w,w,w,w]
+        ]),
+        I: new Sprite([
+            [w,n,n,n,w],
+            [w,w,w,w,w],
+            [w,n,n,n,w]
+        ]),
+        J: new Sprite([
+            [n,n,n,w,n],
+            [n,n,n,n,w],
+            [w,w,w,w,n]
+        ]),
+        K: new Sprite([
+            [w,w,w,w,w],
+            [n,n,w,n,n],
+            [n,w,n,w,w]
+        ]),
+        L: new Sprite([
+            [w,w,w,w,w],
+            [n,n,n,n,w],
+            [n,n,n,n,w]
+        ]),
+        M: new Sprite([
+            [w,w,w,w,w],
+            [n,w,n,n,n],
+            [n,n,w,n,n],
+            [n,w,n,n,n],
+            [w,w,w,w,w]
+        ]),
+        N: new Sprite([
+            [w,w,w,w,w],
+            [n,w,n,n,n],
+            [n,n,w,n,n],
+            [w,w,w,w,w]
+        ]),
+        O: new Sprite([
+            [n,w,w,w,n],
+            [w,n,n,n,w],
+            [n,w,w,w,n]
+        ]),
+        P: new Sprite([
+            [w,w,w,w,w],
+            [w,n,w,n,n],
+            [n,w,n,n,n]
+        ]),
+        Q: new Sprite([
+            [n,w,w,w,n],
+            [w,n,n,n,w],
+            [n,w,w,w,w]
+        ]),
+        R: new Sprite([
+            [w,w,w,w,w],
+            [w,n,w,n,n],
+            [n,w,n,w,w]
+        ]),
+        S: new Sprite([
+            [n,w,n,n,w],
+            [w,n,w,n,w],
+            [w,n,n,w,n]
+        ]),
+        T: new Sprite([
+            [w,n,n,n,n],
+            [w,w,w,w,w],
+            [w,n,n,n,n]
+        ]),
+        U: new Sprite([
+            [w,w,w,w,n],
+            [n,n,n,n,w],
+            [w,w,w,w,w]
+        ]),
+        V: new Sprite([
+            [w,w,w,w,n],
+            [n,n,n,n,w],
+            [w,w,w,w,n]
+        ]),
+        W: new Sprite([
+            [w,w,w,w,n],
+            [n,n,n,n,w],
+            [n,n,n,w,n],
+            [n,n,n,n,w],
+            [w,w,w,w,n]
+        ]),
+        X: new Sprite([
+            [w,w,n,w,w],
+            [n,n,w,n,n],
+            [w,w,n,w,w]
+        ]),
+        Y: new Sprite([
+            [w,w,n,n,n],
+            [n,n,w,n,w],
+            [w,w,w,w,n]
+        ]),
+        Z: new Sprite([
+            [w,n,n,w,w],
+            [w,n,w,n,w],
+            [w,w,n,n,w]
+        ]),
+
 
 
         ' ': new Sprite([
