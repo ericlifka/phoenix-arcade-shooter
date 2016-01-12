6832fa05affcfed370139f61934643f58b3ba249
Eric Lifka <eric.lifka@gmail.com>
2016-01-11 23:35:12 -0500

    adding a new smaller font

---
+++ src/fonts/arcade-small.js
@@ -0,0 +1,17 @@
+DefineModule('fonts/arcade-small', function (require) {
+    var Sprite = require('models/sprite');
+
+    var w = "white";
+    var n = null;
+
+    return {
+        meta: {
+            width: 3,
+            height: 5,
+            lineHeight: 7,
+            letterSpacing: 1,
+            credit: "me"
+        },
+
+    };
+});
