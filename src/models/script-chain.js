424f8546a3e1778446fd75d0ab3f893b40421a25
Eric Lifka <eric.lifka@gmail.com>
2016-01-13 23:30:34 -0500

    new concept, multiple scripts wrapped in a script chain, like multiple sprites wrapped in a sprite group

---
+++ src/models/script-chain.js
@@ -0,0 +1,8 @@
+DefineModule('models/script-chain', function (require) {
+    var GameObject = require('models/game-object');
+    return DefineClass(GameObject, {
+        constructor: function (parent, scripts) {
+
+        }
+    });
+});
