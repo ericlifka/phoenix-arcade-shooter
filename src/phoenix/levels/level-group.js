234ab9cabac1087a46ad584e370908e65c60841c
Eric Lifka <eric.lifka@gmail.com>
2016-01-14 19:19:37 -0500

    replace the parent in the chian

--- src/phoenix/levels/level-group.js
+++ src/phoenix/levels/level-group.js
@@ -2,6 +2,13 @@ DefineModule('phoenix/levels/level-group', function (require) {
     var GameObject = require('model/game-object');
 
     return DefineClass(GameObject, {
-        
+        constructor: function (parent, levels) {
+            this.super('constructor', arguments);
+
+            var self = this;
+            levels.forEach(function (level) {
+                level.parent = self;
+            });
+        }
     });
 });
