d98c66c84c252689cd1cbfa6d797791bc3547a15
Eric Lifka <eric.lifka@gmail.com>
2016-01-14 19:12:19 -0500

    defineclass

--- src/phoenix/levels/level-group.js
+++ src/phoenix/levels/level-group.js
@@ -1,1 +1,7 @@
-DefineModule('phoenix/levels/level-group', function (require) {});
+DefineModule('phoenix/levels/level-group', function (require) {
+    var GameObject = require('model/game-object');
+
+    return DefineClass(GameObject, {
+        
+    });
+});
