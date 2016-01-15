b09cb74d39b24a39b119ba04166422c14d45fb5a
Eric Lifka <eric.lifka@gmail.com>
2016-01-15 18:03:08 -0500

    default is green bar

--- src/components/life-meter.js
+++ src/components/life-meter.js
@@ -1,5 +1,7 @@
 DefineModule('components/life-meter', function (require) {
     var GameObject = require('models/game-object');
+    var Sprite = require('models/sprite');
+    var g = "#2D882D";
 
     return DefineClass(GameObject, {
         constructor: function (parent, boundEntity, position) {
@@ -7,6 +9,11 @@ DefineModule('components/life-meter', function (require) {
 
             this.entity = boundEntity;
             this.position = position;
+
+            this.sprite = new Sprite([
+                [g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g],
+                [g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g, g]
+            ]);
         },
 
         update: function () {
