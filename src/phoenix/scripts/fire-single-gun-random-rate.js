86930808f0e6b56dd95da808b7c3b35052731ad1
Eric Lifka <eric.lifka@gmail.com>
2016-01-15 13:29:37 -0500

    i'm building up a set of boilerplate, and I don't know how I feel about that

--- src/phoenix/scripts/fire-single-gun-random-rate.js
+++ src/phoenix/scripts/fire-single-gun-random-rate.js
@@ -1,3 +1,11 @@
 DefineModule('phoenix/scripts/fire-single-gun-random-rate', function (require) {
-    return DefineClass({});
+    var GameObject = require('models/game-object');
+
+    return DefineClass(GameObject, {
+        constructor: function (parent, ship) {
+            this.super('constructor', arguments);
+
+            this.ship = ship;
+        }
+    });
 });
