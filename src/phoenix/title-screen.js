6f3d008143724136fbcae4bbb35b11ca4d86d5cf
Eric Lifka <eric.lifka@gmail.com>
2016-01-16 15:10:53 -0500

    some default behavior

--- src/phoenix/title-screen.js
+++ src/phoenix/title-screen.js
@@ -1,5 +1,19 @@
 DefineModule('phoenix/title-screen', function (require) {
     var GameObject = require('models/game-object');
+    var TextDisplay = require('components/text-display');
 
-    return DefineClass(GameObject, {});
+    return DefineClass(GameObject, {
+        constructor: function (parent) {
+            this.super('constructor', arguments);
+
+            this.addChild(new TextDisplay(this, {
+                message: "PHOENIX",
+                position: { x: 55, y: 50 },
+                border: true,
+                padding: 15,
+                color: colorGradient[ this.colorIndex ],
+                font: "arcade"
+            }));
+        }
+    });
 });
