03f1ff2743b4f47b538b8f7daf6346a2dc9c3a1b
Eric Lifka <eric.lifka@gmail.com>
2016-01-16 18:59:07 -0500

    menu selectors, everything is still static

--- src/phoenix/title-screen.js
+++ src/phoenix/title-screen.js
@@ -1,8 +1,11 @@
 DefineModule('phoenix/title-screen', function (require) {
     var GameObject = require('models/game-object');
     var TextDisplay = require('components/text-display');
+    var ArrowShip = require('phoenix/sprites/arrow-ship');
 
     return DefineClass(GameObject, {
+        selectedMenuItem: 0,
+
         constructor: function (parent) {
             this.super('constructor', arguments);
 
@@ -23,6 +26,18 @@ DefineModule('phoenix/title-screen', function (require) {
                 message: "Load",
                 position: { x: 95, y: 95 }
             }));
+
+            this.selectorLeft = new GameObject();
+            this.selectorRight = new GameObject();
+
+            this.selectorLeft.sprite = new ArrowShip();
+            this.selectorRight.sprite = new ArrowShip().invertX();
+
+            this.selectorLeft.position = { x: 75, y: 80 };
+            this.selectorRight.position = { x: 120, y: 80 };
+
+            this.addChild(this.selectorLeft);
+            this.addChild(this.selectorRight);
         }
     });
 });
