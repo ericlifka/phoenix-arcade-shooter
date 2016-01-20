9ee183e8aeee2f0cd6d035d0f18a02a67582a45c
Eric Lifka <eric.lifka@gmail.com>
2016-01-19 21:23:33 -0500

    simple boss sprite

---
+++ src/phoenix/sprites/arrow-boss.js
@@ -0,0 +1,21 @@
+DefineModule('phoenix/sprites/arrow-boss', function (require) {
+    var Sprite = require('models/sprite');
+
+    return function () {
+        var w = "white";
+        var n = null;
+        return new Sprite([
+            [ w, n, n, n, n, n, w, n, w, n, n, n, n, n, w ],
+            [ w, w, n, n, n, w, w, w, w, w, n, n, n, w, w ],
+            [ n, w, w, n, w, w, w, w, w, w, w, n, w, w, n ],
+            [ n, w, w, w, w, w, w, w, w, w, w, w, w, w, n ],
+            [ n, n, w, w, w, w, w, w, w, w, w, w, w, n, n ],
+            [ n, n, w, w, w, w, w, w, w, w, w, w, w, n, n ],
+            [ n, n, n, w, n, w, w, w, w, w, n, w, n, n, n ],
+            [ n, n, n, w, n, n, w, w, w, n, n, w, n, n, n ],
+            [ n, n, n, n, n, n, n, w, n, n, n, n, n, n, n ],
+            [ n, n, n, n, n, n, n, w, n, n, n, n, n, n, n ],
+            [ n, n, n, n, n, n, n, w, n, n, n, n, n, n, n ]
+        ]);
+    }
+});
