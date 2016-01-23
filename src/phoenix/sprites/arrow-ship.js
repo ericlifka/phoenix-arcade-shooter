19499ca378de37f7a767af59aa13212da058dda5
Eric Lifka <eric.lifka@gmail.com>
2016-01-22 22:09:25 -0500

    give the arrow ship a bold new look in greyscale

--- src/phoenix/sprites/arrow-ship.js
+++ src/phoenix/sprites/arrow-ship.js
@@ -2,22 +2,26 @@ DefineModule('phoenix/sprites/arrow-ship', function (require) {
     var Sprite = require('models/sprite');
 
     return function () {
-        var w = "white";
-        var n = null;
+        var w1 = "#ffffff";
+        var w2 = "#cccccc";
+        var g1 = "#aaaaaa";
+        var g2 = "#888888";
+        var g3 = "#666666";
+        var nn = null;
         return new Sprite([
-            [ w, n, n, n, n, n, w ],
-            [ w, w, n, n, n, w, w ],
-            [ n, w, w, n, w, w, n ],
-            [ n, w, w, w, w, w, n ],
-            [ n, n, w, w, w, n, n ],
-            [ n, n, w, w, w, n, n ],
-            [ n, n, n, w, n, n, n ],
-            [ n, n, n, w, n, n, n ]
-        ],
-        {
-            guns: [
-                { x: 3, y: 7 }
-            ]
-        });
+                [ g3, nn, nn, nn, nn, nn, g3 ],
+                [ g2, g2, nn, nn, nn, g2, g2 ],
+                [ nn, g2, g1, nn, g1, g2, nn ],
+                [ nn, g1, g1, w1, g1, g1, nn ],
+                [ nn, nn, w2, w1, w2, nn, nn ],
+                [ nn, nn, w2, w1, w2, nn, nn ],
+                [ nn, nn, nn, w1, nn, nn, nn ],
+                [ nn, nn, nn, w1, nn, nn, nn ]
+            ],
+            {
+                guns: [
+                    { x: 3, y: 7 }
+                ]
+            });
     };
 });
