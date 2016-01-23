350cd8e5397c373b12fce4c28926d3ab44006d9a
Eric Lifka <eric.lifka@gmail.com>
2016-01-22 22:33:45 -0500

    add a cockpit pixel

--- src/phoenix/sprites/arrow-ship.js
+++ src/phoenix/sprites/arrow-ship.js
@@ -7,13 +7,14 @@ DefineModule('phoenix/sprites/arrow-ship', function (require) {
         var g1 = "#aaaaaa";
         var g2 = "#888888";
         var g3 = "#666666";
+        var g4 = "#222222";
         var nn = null;
         return new Sprite([
                 [ g3, nn, nn, nn, nn, nn, g3 ],
                 [ g2, g2, nn, nn, nn, g2, g2 ],
                 [ nn, g2, g1, nn, g1, g2, nn ],
                 [ nn, g1, g1, w1, g1, g1, nn ],
-                [ nn, nn, w2, w1, w2, nn, nn ],
+                [ nn, nn, w2, g4, w2, nn, nn ],
                 [ nn, nn, w2, w1, w2, nn, nn ],
                 [ nn, nn, nn, w1, nn, nn, nn ],
                 [ nn, nn, nn, w1, nn, nn, nn ]
