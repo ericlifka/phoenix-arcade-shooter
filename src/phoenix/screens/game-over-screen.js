b2b2dd523b8b6a9359bacecacad33abe8682582e
Eric Lifka <eric.lifka@gmail.com>
2016-01-23 14:44:34 -0500

    convert game-over-screen to evented-input style, much simpler

--- src/phoenix/screens/game-over-screen.js
+++ src/phoenix/screens/game-over-screen.js
@@ -1,13 +1,11 @@
 DefineModule('phoenix/game-over-screen', function (require) {
+    var EventedInput = require('models/evented-input');
     var GameObject = require('models/game-object');
     var TextDisplay = require('components/text-display');
 
     return DefineClass(GameObject, {
         reset: function () {
             this.super('reset');
-
-            this.inputReleased = false;
-            this.resetPressed = false;
 
             this.addChild(new TextDisplay(this, {
                 font: "arcade",
@@ -24,16 +22,14 @@ DefineModule('phoenix/game-over-screen', function (require) {
                 color: "red",
                 position: { x: 75, y: 81 }
             }));
+
+            this.addChild(new EventedInput({
+                onSelect: this.onSelect.bind(this)
+            }));
         },
 
-        processInput: function (input) {
-            if (!input.menuSelect && !input.fire) {
-                this.inputReleased = true;
-            }
-            if (!this.resetPressed && this.inputReleased && (input.menuSelect || input.fire)) {
-                this.resetPressed = true;
-                this.parent.reset();
-            }
+        onSelect: function () {
+            this.parent.reset();
         }
     })
 });
