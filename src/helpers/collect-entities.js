96aaaa9a10e7913bbee6dfd8dc26b136e40544c9
Eric Lifka <eric.lifka@gmail.com>
2016-01-24 13:37:18 -0500

    simple depth first search

--- src/helpers/collect-entities.js
+++ src/helpers/collect-entities.js
@@ -1,5 +1,19 @@
 DefineModule('helpers/collect-entities', function () {
-    return function (headNode) {
+    return function visitNode(node, collection) {
+        collection = collection || [];
 
+        if (node) {
+            if (node.isPhysicalEntity) {
+                collection.push(node);
+            }
+
+            if (node.children && node.children.length) {
+                for (var i = 0; i < node.children.length; i++) {
+                    visitNode(node.children[i], collection);
+                }
+            }
+        }
+
+        return collection;
     };
 });
