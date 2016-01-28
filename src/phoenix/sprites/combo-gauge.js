23abb0334f3e11863d4525f46d0153d6207dac8a
Eric Lifka <eric.lifka@gmail.com>
2016-01-28 00:45:52 -0500

    new sprite

---
+++ src/phoenix/sprites/combo-gauge.js
@@ -0,0 +1,10 @@
+DefineModule('phoenix/sprites/combo-gauge', function (require) {
+    var Sprite = require('models/sprite');
+
+    return function () {
+        var n = null;
+        return new Sprite([
+            []
+        ]);
+    };
+});
