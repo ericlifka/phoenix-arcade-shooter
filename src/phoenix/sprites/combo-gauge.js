9589915be38394ba6d1d1b0647c95c9338e28a6b
Eric Lifka <eric.lifka@gmail.com>
2016-01-28 00:48:59 -0500

    first pass

--- src/phoenix/sprites/combo-gauge.js
+++ src/phoenix/sprites/combo-gauge.js
@@ -2,9 +2,15 @@ DefineModule('phoenix/sprites/combo-gauge', function (require) {
     var Sprite = require('models/sprite');
 
     return function () {
+        var w = "#000";
         var n = null;
         return new Sprite([
-            []
+            [w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w],
+            [w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w],
+            [w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w],
+            [w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w],
+            [w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w,n,n,n,n,w],
+            [w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w]
         ]);
     };
 });
