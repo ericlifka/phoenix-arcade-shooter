239fe9704b560ba2272571838f0956e10195a9f6
Eric Lifka <eric.lifka@gmail.com>
2016-02-02 13:54:16 -0500

    first pass at deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+tar cvzf build.tar.gz index.html modules.js src/
+
+git co gh-pages
+
+rm -r src/
+rm index.html
+rm modules.js
+
+tar -xf build.tar.gz
+rm build.tar.gz
+
+git add --all
+git commit -m "auto build"
+git push
+
+git co master
