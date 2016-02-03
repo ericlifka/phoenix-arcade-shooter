c801e46393a8f4aa126cf1485566af51027f2a55
Eric Lifka <eric.lifka@gmail.com>
2016-02-03 18:55:13 -0500

    rgb to hsv converter

--- src/helpers/gradients.js
+++ src/helpers/gradients.js
@@ -1,13 +1,32 @@
 DefineModule('helpers/gradients', function (require) {
+    function RGBtoHSV(color) {
+        var R = color.R;
+        var G = color.G;
+        var B = color.B;
 
-    function calcChroma(color) {
-        var M = Math.max(color.R, color.G, color.B);
-        var m = Math.min(color.R, color.G, color.B);
+        var M = Math.max(R, G, B);
+        var m = Math.min(R, G, B);
+        var C = M - m;
 
-        return M - m;
+        var Hp = 0;
+        if (M === R) {
+            Hp = (G - B) / C % 6;
+        } else if (M === G) {
+            Hp = (B - R) / C + 2;
+        } else if (M === B) {
+            Hp = (R - G) / C + 4;
+        }
+
+        var H = Hp * 60;
+        var L = .5 * (M + m);
+        var S = C / V;
+
+        return {
+            H: H,
+            S: S,
+            L: L
+        };
     }
-
-
 
     return {
         GreenToRed: [ "#0AF448", "#0BF440", "#0BF437", "#0CF42E", "#0CF426",
