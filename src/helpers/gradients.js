d3ca2fdbf2541053455842ae3f0c59a09ecfdbc9
Eric Lifka <eric.lifka@gmail.com>
2016-02-03 22:16:27 -0500

    don't need a converter, can bring back if it becomes useful

--- src/helpers/gradients.js
+++ src/helpers/gradients.js
@@ -1,33 +1,4 @@
-DefineModule('helpers/gradients', function (require) {
-    function RGBtoHSV(color) {
-        var R = color.R;
-        var G = color.G;
-        var B = color.B;
-
-        var M = Math.max(R, G, B);
-        var m = Math.min(R, G, B);
-        var C = M - m;
-
-        var Hp = 0;
-        if (M === R) {
-            Hp = (G - B) / C % 6;
-        } else if (M === G) {
-            Hp = (B - R) / C + 2;
-        } else if (M === B) {
-            Hp = (R - G) / C + 4;
-        }
-
-        var H = Hp * 60;
-        var L = .5 * (M + m);
-        var S = C / V;
-
-        return {
-            H: H,
-            S: S,
-            L: L
-        };
-    }
-
+DefineModule('helpers/gradients', function () {
     return {
         GreenToRed: {
             start: 120,
