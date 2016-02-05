797ed371749f992a206a6c01376b0b6d915073c4
Eric Lifka <eric.lifka@gmail.com>
2016-02-04 22:58:34 -0500

    disable some stuff

--- src/views/webgl-renderer.js
+++ src/views/webgl-renderer.js
@@ -52,7 +52,7 @@ DefineModule('views/webgl-renderer', function (require) {
             this.canvas = createCanvasEl(this);
             this.container.appendChild(this.canvas);
 
-            this.canvasDrawContext = this.canvas.getContext("2d", { alpha: false });
+            this.canvasDrawContext = this.canvas.getContext("webgl");
             this.frames = [
                 new Frame(this),
                 new Frame(this)
@@ -63,25 +63,25 @@ DefineModule('views/webgl-renderer', function (require) {
             return this.frames[ this.nextFrame ];
         },
         renderFrame: function () {
-            var frame = this.frames[ this.nextFrame ];
-            var pixelSize = this.pixelSize;
-            var ctx = this.canvasDrawContext;
-            var fillColor = frame.fillColor;
-
-            ctx.fillStyle = fillColor;
-            ctx.fillRect(0, 0, this.fullWidth, this.fullHeight);
-
-            frame.iterateCells(function (cell, x, y) {
-                if (cell.color !== fillColor) {
-                    ctx.beginPath();
-                    ctx.rect(cell.render_x, cell.render_y, pixelSize, pixelSize);
-                    ctx.fillStyle = cell.color;
-                    ctx.fill();
-                    ctx.closePath();
-                }
-            });
-
-            this.nextFrame = +!this.nextFrame; // switch the frames
+            //var frame = this.frames[ this.nextFrame ];
+            //var pixelSize = this.pixelSize;
+            //var ctx = this.canvasDrawContext;
+            //var fillColor = frame.fillColor;
+            //
+            //ctx.fillStyle = fillColor;
+            //ctx.fillRect(0, 0, this.fullWidth, this.fullHeight);
+            //
+            //frame.iterateCells(function (cell, x, y) {
+            //    if (cell.color !== fillColor) {
+            //        ctx.beginPath();
+            //        ctx.rect(cell.render_x, cell.render_y, pixelSize, pixelSize);
+            //        ctx.fillStyle = cell.color;
+            //        ctx.fill();
+            //        ctx.closePath();
+            //    }
+            //});
+            //
+            //this.nextFrame = +!this.nextFrame; // switch the frames
         },
         setFillColor: function (fillColor) {
             this.frames.forEach(function (frame) {
