baef2d90844ebeecc6670787e7906071ed33dd91
Eric Lifka <eric.lifka@gmail.com>
2016-02-05 21:12:11 -0500

    this is sprialling out of control

--- src/views/webgl-renderer.js
+++ src/views/webgl-renderer.js
@@ -37,6 +37,40 @@ DefineModule('views/webgl-renderer', function (require) {
 
     var horizAspect = 480.0/640.0;
 
+    function loadIdentity() {
+        mvMatrix = Matrix.I(4);
+    }
+
+    function multMatrix(m) {
+        mvMatrix = mvMatrix.x(m);
+    }
+
+    function mvTranslate(v) {
+        multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+    }
+
+    function setMatrixUniforms() {
+        var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+        gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
+
+        var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+        gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+    }
+
+    function drawScene(gl) {
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
+
+        loadIdentity();
+        mvTranslate([-0.0, 0.0, -6.0]);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+        gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+        setMatrixUniforms();
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    }
+
     function initBuffers(gl) {
         squareVerticesBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
