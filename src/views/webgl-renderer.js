671a94b2acb42bd75a0acb9a2cf5a7ce3de6764e
Eric Lifka <eric.lifka@gmail.com>
2016-02-05 21:07:46 -0500

    functions for setting up shaders, stolen from mdn tutorial

--- src/views/webgl-renderer.js
+++ src/views/webgl-renderer.js
@@ -35,6 +35,72 @@ DefineModule('views/webgl-renderer', function (require) {
         return el;
     }
 
+    function getShader(gl, id) {
+        var shaderScript, theSource, currentChild, shader;
+
+        shaderScript = document.getElementById(id);
+
+        if (!shaderScript) {
+            return null;
+        }
+
+        theSource = "";
+        currentChild = shaderScript.firstChild;
+
+        while (currentChild) {
+            if (currentChild.nodeType == currentChild.TEXT_NODE) {
+                theSource += currentChild.textContent;
+            }
+
+            currentChild = currentChild.nextSibling;
+        }
+
+        if (shaderScript.type == "x-shader/x-fragment") {
+            shader = gl.createShader(gl.FRAGMENT_SHADER);
+        } else if (shaderScript.type == "x-shader/x-vertex") {
+            shader = gl.createShader(gl.VERTEX_SHADER);
+        } else {
+            // Unknown shader type
+            return null;
+        }
+
+        gl.shaderSource(shader, theSource);
+
+        // Compile the shader program
+        gl.compileShader(shader);
+
+        // See if it compiled successfully
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+            return null;
+        }
+
+        return shader;
+    }
+
+    function initShaders(gl) {
+        var fragmentShader = getShader(gl, "shader-fs");
+        var vertexShader = getShader(gl, "shader-vs");
+
+        // Create the shader program
+
+        shaderProgram = gl.createProgram();
+        gl.attachShader(shaderProgram, vertexShader);
+        gl.attachShader(shaderProgram, fragmentShader);
+        gl.linkProgram(shaderProgram);
+
+        // If creating the shader program failed, alert
+
+        if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+            alert("Unable to initialize the shader program.");
+        }
+
+        gl.useProgram(shaderProgram);
+
+        vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+        gl.enableVertexAttribArray(vertexPositionAttribute);
+    }
+
     return DefineClass({
         width: 80,
         height: 50,
