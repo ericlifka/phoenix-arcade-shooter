e69a1f363d4a656612deddb205b51b10b18a3f36
Eric Lifka <eric.lifka@gmail.com>
2016-03-24 14:52:42 -0400

    add html building so that js src links are sorted

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const htmlbuild = require('gulp-htmlbuild');
 
 gulp.task('default', function () {
+    gulp.src([ './index.html' ])
+        .pipe(htmlbuild({
+            js: htmlbuild.preprocess.js(function (block) {
+                block.write('phoenix-arcade-shooter.js');
+                block.end();
+            })
+        }))
+        .pipe(gulp.dest('./dist'));
+
     return gulp.src([ './modules.js', './src/**/*.js' ])
         .pipe(concat({ path: 'phoenix-arcade-shooter.js' }))
         .pipe(gulp.dest('./dist'));
