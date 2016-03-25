ce90b4d30b71ad7b69d9acd9872900e77172c962
Eric Lifka <eric.lifka@gmail.com>
2016-03-25 00:08:57 -0400

    throw a wrapper around the whole built file to minimize leakage

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ const concat = require('gulp-concat');
 const filter = require('gulp-filter');
 const htmlbuild = require('gulp-htmlbuild');
 
+const wrapper = require('gulp-wrapper')({
+    header: '(function () {\n',
+    footer: '}());\n'
+});
+
 gulp.task('default', function () {
     gulp.src([ './index.html', './styles/game.css', './favicon.ico' ])
         .pipe(htmlbuild({
@@ -20,10 +25,12 @@ gulp.task('default', function () {
     gulp.src([ './modules.js', './src/**/*.js' ])
         .pipe(filter(function (file) { return !/embedded/.test(file.path) }))
         .pipe(concat({ path: 'phoenix-arcade-shooter.js' }))
+        .pipe(wrapper)
         .pipe(gulp.dest('./dist'));
 
     return gulp.src([ './modules.js', './src/**/*.js' ])
         .pipe(filter(function (file) { return !/main/.test(file.path) }))
         .pipe(concat({ path: 'phoenix-arcade-shooter-embedded.js' }))
+        .pipe(wrapper)
         .pipe(gulp.dest('./dist'));
 });
