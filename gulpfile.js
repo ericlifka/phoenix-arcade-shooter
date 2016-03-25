5860c01809f91667f30a8440a36ec4f594e4e886
Eric Lifka <eric.lifka@gmail.com>
2016-03-24 22:29:28 -0400

    figured out filtering so that two versions of the source can be generated to support an embedded version of the game

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const filter = require('gulp-filter');
 const htmlbuild = require('gulp-htmlbuild');
 
 gulp.task('default', function () {
@@ -17,6 +18,7 @@ gulp.task('default', function () {
         .pipe(gulp.dest('./dist'));
 
     return gulp.src([ './modules.js', './src/**/*.js' ])
+        .pipe(filter(function (file) { console.log(file.path); return !/embedded-main/.test(file.path) }))
         .pipe(concat({ path: 'phoenix-arcade-shooter.js' }))
         .pipe(gulp.dest('./dist'));
 });
---
+++ src/main-embedded.js
@@ -0,0 +1,1 @@
+alert("SHOULDN'T SHOW UP");
