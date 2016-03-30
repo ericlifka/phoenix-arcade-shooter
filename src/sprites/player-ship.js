0b6064a749628ecf0a98b82741f89ecb8fa2270f
Eric Lifka <eric.lifka@gmail.com>
2016-03-30 01:54:18 -0400

    remove the wing guns from the player's default sprite

--- src/sprites/player-ship.js
+++ src/sprites/player-ship.js
@@ -9,8 +9,8 @@ DefineModule('sprites/player-ship', function (require) {
             [ n, n, n, w, n, n, n ],
             [ n, n, w, w, w, n, n ],
             [ n, n, w, w, w, n, n ],
-            [ w, n, w, w, w, n, w ],
-            [ w, n, w, w, w, n, w ],
+            [ n, n, w, w, w, n, n ],
+            [ n, w, w, w, w, w, n ],
             [ w, w, w, w, w, w, w ],
             [ n, n, w, w, w, n, n ],
             [ n, n, n, w, n, n, n ]
